4559894b3b225c6364be0d0f0c876eed4a7b09a5
No_way <oncwnuG3VdUbD1_vFnzuTZ0hGCEY@git.weixin.qq.com>
2022-05-06 19:33:54 +0800

    优化了路径

--- index/index.js
+++ index/index.js
@@ -2,22 +2,22 @@
 window.onload = function () {
   var jump1 = document.getElementById("jump1")
   jump1.onclick = function () {
-    location.href = "/pages/page1/page1.html"
+    location.href = "../pages/page1/page1.html"
   }
   var jump2 = document.getElementById("jump2")
   jump2.onclick = function () {
-    location.href = "/pages/page2/page2.html"
+    location.href = "../pages/page2/page2.html"
   }
   var jump3 = document.getElementById("jump3")
   jump3.onclick = function () {
-    location.href = "/pages/page3/page3.html"
+    location.href = "../pages/page3/page3.html"
   }
   var jump4 = document.getElementById("jump4")
   jump4.onclick = function () {
-    location.href = "/pages/page4/page4.html"
+    location.href = "../pages/page4/page4.html"
   }
   var jump5 = document.getElementById("jump5")
   jump5.onclick = function () {
-    location.href = "/pages/page5/page5.html"
+    location.href = "../pages/page5/page5.html"
   }
 }
