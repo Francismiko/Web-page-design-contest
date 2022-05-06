991cb980340a78ab90642c7c1b49eac8e6b38cd0
No_way <oncwnuG3VdUbD1_vFnzuTZ0hGCEY@git.weixin.qq.com>
2022-05-06 23:13:11 +0800

    优化代码

--- index/index.js
+++ index/index.js
@@ -2,22 +2,27 @@
 window.onload = function () {
   var jump1 = document.getElementById("jump1")
   jump1.onclick = function () {
-    location.href = "../pages/page1/page1.html"
+    window.open("../pages/page1/page1.html")
+    console.log("用户点击了'测试例'跳转链接")
   }
   var jump2 = document.getElementById("jump2")
   jump2.onclick = function () {
-    location.href = "../pages/page2/page2.html"
+    window.open("../pages/page2/page2.html")
+    console.log("用户点击了'测试例'跳转链接")
   }
   var jump3 = document.getElementById("jump3")
   jump3.onclick = function () {
-    location.href = "../pages/page3/page3.html"
+    window.open("../pages/page3/page3.html")
+    console.log("用户点击了'测试例'跳转链接")
   }
   var jump4 = document.getElementById("jump4")
   jump4.onclick = function () {
-    location.href = "../pages/page4/page4.html"
+    window.open("../pages/page4/page4.html")
+    console.log("用户点击了'测试例'跳转链接")
   }
   var jump5 = document.getElementById("jump5")
   jump5.onclick = function () {
-    location.href = "../pages/page5/page5.html"
+    window.open("../pages/page5/page5.html")
+    console.log("用户点击了'测试例'跳转链接")
   }
 }
